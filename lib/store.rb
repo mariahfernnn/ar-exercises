c7055d2d32b1f91f4701c5f7fc98f5092ba913dd
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-13 09:35:40 -0500

    Exercise 7 complete

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 2 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
 end
+
+# Store.create(name: "ab").valid?
+# Store.create(annual_revenue: nil).valid?
